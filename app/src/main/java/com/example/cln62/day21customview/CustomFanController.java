ebcbb79ce7a945ef9fe0567b40495a6f666722b3
cln62 <cln62@mst.edu>
2018-10-23 19:07:48 -0500

    Initial commit

---
+++ app/src/main/java/com/example/cln62/day21customview/CustomFanController.java
@@ -0,0 +1,13 @@
+package com.example.cln62.day21customview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomFanController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_fan_controller);
+    }
+}
